94a400ce494658507016d4eeb5548788dadacf56
Deeljore <bdeeljore@gmail.com>
2019-10-14 11:18:09 +0200

    creation angular

---
+++ FilRougeAngular/CaseOncleSeraphinFront/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { DescriptionComponent } from './description/description.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DescriptionComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
