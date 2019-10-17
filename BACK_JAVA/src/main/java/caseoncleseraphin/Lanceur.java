306e6d64e45419c0c02df302055cffc3b8988783
Pierre8488 <giraud.pierre.pro@gmail>
2019-10-17 15:07:26 +0200

    back

--- BACK_JAVA/src/main/java/caseoncleseraphin/Lanceur.java
+++ BACK_JAVA/src/main/java/caseoncleseraphin/Lanceur.java
@@ -36,47 +36,51 @@ public class Lanceur {
 	@PostConstruct
 	public void initializeData() throws BadRequestException, NotFoundException {
 		if (userService.findAll().isEmpty()) {
-			//TODO: Création des droits
+			
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Creation des droits 
+			
+			//Droits client
 			Right createCommand = new Right("C_COMMAND");
 			rightService.save(createCommand);
 			Right updateCommand = new Right("U_COMMAND");
 			rightService.save(updateCommand);
 			Right deleteCommand = new Right("D_COMMAND");
 			rightService.save(deleteCommand);
+			
+			//Droits Admin
 			Right createProduct = new Right("C_PRODUCT");
 			rightService.save(createProduct);
 			Right updateProduct = new Right("U_PRODUCT");
 			rightService.save(updateProduct);
 			Right deleteProduct = new Right("D_PRODUCT");
 			rightService.save(deleteProduct);
-			
 			Right createCategory = new Right("C_CATEGORY");
 			rightService.save(createCategory);
 			Right updateCategory = new Right("U_CATEGORY");
 			rightService.save(updateCategory);
 			Right deleteCategory = new Right("D_CATEGORY");
 			rightService.save(deleteCategory);
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////			
+//Creation des rôles et attribution des droits aux rôles
 			
-			
-			//TODO: Création des rôle + attribution des droits aux rôles
+			//Creation des droits admin
 			List<Right> adminRights = Arrays.asList(createProduct,updateProduct,deleteProduct,createCategory,updateCategory,deleteCategory);
 			Role roleAdmin = new Role("ADMIN");
 			roleAdmin.setRights(adminRights);
 			roleService.save(roleAdmin);
 
-			
+			//Creation des droits client
 			List<Right> customerRights = Arrays.asList(createCommand,updateCommand,deleteCommand);
 			Role roleCustomer = new Role("CUSTOMER");
 			roleCustomer.setRights(customerRights);
 			roleService.save(roleCustomer);
-			
-			
-
-			//TODO: Création des users + attribution du rôle à chaque user
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////			
+//Creation des users et attribution d'un rôle pour chaque user
+			//Creation des admins
 			
 			User admin1 = new User("Deeljore", "Brice", "bdeeljore", "bdeeljore@gmail.com", "7 rue de la resistance",
 					"0699842032", "azerty", (LocalDate.of(2019, 9, 21)), roleAdmin);
-
 			try {
 				userService.findOneByUsername("bdeeljore");
 				System.out.println("le login " + admin1.getUsername() + " existe déjà.");
@@ -85,7 +89,36 @@ public class Lanceur {
 				System.out.println("l'utilisateur " + admin1.getUsername() + " a ete créé.");
 			}
 			
+			User admin2 = new User("Giraud", "Pierre", "pgiraud", "pgiraud@gmail.com", "8 rue de la resistance",
+					"0699842033", "azerty", (LocalDate.of(2019, 9, 21)), roleAdmin);
+			try {
+				userService.findOneByUsername("bdeeljore");
+				System.out.println("le login " + admin2.getUsername() + " existe déjà.");
+			} catch (EmptyResultDataAccessException e) {
+				userService.save(admin2);
+				System.out.println("l'utilisateur " + admin2.getUsername() + " a ete créé.");
+			}
 			
+			User admin3 = new User("Augier", "Julien", "jaugier", "jaugier@gmail.com", "9 rue de la resistance",
+					"0699842034", "azerty", (LocalDate.of(2019, 9, 21)), roleAdmin);
+			try {
+				userService.findOneByUsername("bdeeljore");
+				System.out.println("le login " + admin3.getUsername() + " existe déjà.");
+			} catch (EmptyResultDataAccessException e) {
+				userService.save(admin3);
+				System.out.println("l'utilisateur " + admin3.getUsername() + " a ete créé.");
+			}
+			User admin4 = new User("Saint-Martin", "Guillaume", "gsaintmartin", "gsaintmartin@gmail.com", "10 rue de la resistance",
+					"0699842035", "azerty", (LocalDate.of(2019, 9, 21)), roleAdmin);
+			try {
+				userService.findOneByUsername("bdeeljore");
+				System.out.println("le login " + admin4.getUsername() + " existe déjà.");
+			} catch (EmptyResultDataAccessException e) {
+				userService.save(admin4);
+				System.out.println("l'utilisateur " + admin4.getUsername() + " a ete créé.");
+			}
+			
+			//Creation des clients
 			User client1 = new User("Duce", "J-C", "jcduce", "jcduce@gmail.com", "8 rue de la resistance",
 					"0699842136", "azert8", (LocalDate.of(2019, 9, 24)), roleCustomer);
 
