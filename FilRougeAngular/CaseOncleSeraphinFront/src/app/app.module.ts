ac2cdbaf04565e3b663c79623e6ee909eea78cd3
Pierre8488 <giraud.pierre.pro@gmail>
2019-10-18 16:53:05 +0200

    on s'accroche aux branches

--- FilRougeAngular/CaseOncleSeraphinFront/src/app/app.module.ts
+++ FilRougeAngular/CaseOncleSeraphinFront/src/app/app.module.ts
@@ -2,9 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CartComponent } from './site/cart/cart.component';
@@ -34,6 +32,33 @@ import { ProductSliderComponent } from './product-slider/product-slider.componen
 
 import { AuthGuardService} from './service/auth-guard.service';
 
+import { AddDigitalProductComponent } from './site/page-admin2/add-digital-product/add-digital-product.component';
+
+import { AddProductComponent } from './site/page-admin2/add-product/add-product.component';
+import { CategoryComponent } from './site/page-admin2/category/category.component';
+import { CategoryDigitalComponent } from './site/page-admin2/category-digital/category-digital.component';
+import { CategoryDigitalsubComponent } from './site/page-admin2/category-digitalsub/category-digitalsub.component';
+import { CouponCreateComponent } from './site/page-admin2/coupon-create/coupon-create.component';
+import { CouponListComponent } from './site/page-admin2/coupon-list/coupon-list.component';
+import { CreateMenuComponent } from './site/page-admin2/create-menu/create-menu.component';
+import { CreateUserComponent } from './site/page-admin2/create-user/create-user.component';
+import { CreateVendorsComponent } from './site/page-admin2/create-vendors/create-vendors.component';
+import { CurrencyRatesComponent } from './site/page-admin2/currency-rates/currency-rates.component';
+import { InvoiceComponent } from './site/page-admin2/invoice/invoice.component';
+import { MediaComponent } from './site/page-admin2/media/media.component';
+import { OrderComponent } from './site/page-admin2/order/order.component';
+import { PageCreateComponent } from './site/page-admin2/page-create/page-create.component';
+import { PagesListComponent } from './site/page-admin2/pages-list/pages-list.component';
+import { ProductDetailComponent } from './site/page-admin2/product-detail/product-detail.component';
+import { ProductListComponent } from './site/page-admin2/product-list/product-list.component';
+import { ProductListdigitalComponent } from './site/page-admin2/product-listdigital/product-listdigital.component';
+import { ProfileComponent } from './site/page-admin2/profile/profile.component';
+import { ReportsComponent } from './site/page-admin2/reports/reports.component';
+import { TaxesComponent } from './site/page-admin2/taxes/taxes.component';
+import { TransactionsComponent } from './site/page-admin2/transactions/transactions.component';
+import { TranslationsComponent } from './site/page-admin2/translations/translations.component';
+import { UserListComponent } from './site/page-admin2/user-list/user-list.component';
+import { IndexAdminComponent } from './site/page-admin2/index-admin/index-admin.component';
 
 
 const appRoutes: Routes = [
@@ -52,7 +77,31 @@ const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'logout', component: LogoutComponent },
   { path: 'not-found', component: Erreur404Component },
-  { path: '**', redirectTo: '/not-found' }
+  { path: '**', redirectTo: '/not-found' },
+
+  // { path: 'add_digital_product', component: AddDigitalProductComponent },
+  // { path: 'add_product', component: AddProductComponent },
+  // { path: 'category', component: CategoryComponent },
+  // { path: 'category_digital', component: CategoryDigitalComponent },
+  // { path: 'category_digitalsub', component: CategoryDigitalsubComponent },
+  // { path: 'coupon_list', component: CouponListComponent },
+  // { path: 'create_menu', component: CreateMenuComponent },
+  // { path: 'create_user', component: CreateUserComponent },
+  // { path: 'create_vendors', component: CreateVendorsComponent },
+  // { path: 'invoice', component: InvoiceComponent },
+  // { path: 'media', component: MediaComponent },
+  // { path: 'page_create', component: PageCreateComponent },
+  // { path: 'pages_list', component: PagesListComponent },
+  // { path: 'product_detail', component: ProductDetailComponent },
+  // { path: 'product_list', component: ProductListComponent },
+  // { path: 'product_listdigital', component: ProductListdigitalComponent },
+  // { path: 'profile', component: ProfileComponent },
+  // { path: 'reports', component: ReportsComponent },
+  // { path: 'taxes', component: TaxesComponent },
+  // { path: 'transactions', component: TransactionsComponent },
+  // { path: 'translations', component: TranslationsComponent },
+  // { path: 'user_list', component: UserListComponent },
+  // { path: 'admin', component: IndexAdminComponent },
 ];
 
 @NgModule({
