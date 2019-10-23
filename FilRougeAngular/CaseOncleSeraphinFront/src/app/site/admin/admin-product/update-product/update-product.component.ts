9f1fb64ec41944537d96f7791d0ab381f777a3fe
Pierre8488 <giraud.pierre.pro@gmail>
2019-10-23 15:34:15 +0200

    ger

--- FilRougeAngular/CaseOncleSeraphinFront/src/app/site/admin/admin-product/update-product/update-product.component.ts
+++ FilRougeAngular/CaseOncleSeraphinFront/src/app/site/admin/admin-product/update-product/update-product.component.ts
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
-import { OnInit, Component } from '@angular/core';
-=======
+
 import {  OnInit, Component } from '@angular/core';
 import { Product } from '../../../../product';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from '../../../../service/product.service';
 
-
->>>>>>> 58f3699155e14dcf05ae36f2599a009edf72c81c
 
 @Component({
   selector: 'app-update-product',
